e9e6ba04a1c1e5b6f1fe979ce95d5b6c336cfaca
Limbureuben <limbureuben4@gmail.com>
2025-04-05 16:10:52 +0300

    changes

--- src/app/sharing/user-home/user-home.component.ts
+++ src/app/sharing/user-home/user-home.component.ts
@@ -8,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class UserHomeComponent {
 
+  title = 'House Rental System';
+  houses = [
+    {
+      name: 'Modern Apartment',
+      location: 'New York, USA',
+      price: '$2000/month',
+      image: 'assets/images/house1.jpg'
+    },
+    {
+      name: 'Luxury Villa',
+      location: 'Los Angeles, USA',
+      price: '$5000/month',
+      image: 'assets/images/house2.jpg'
+    },
+    {
+      name: 'Cozy Cottage',
+      location: 'Austin, USA',
+      price: '$1500/month',
+      image: 'assets/images/house3.jpg'
+    }
+  ];
+
 }
