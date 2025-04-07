7e06f698d92ececeef62df75059041fb59e542b6
Limbureuben <limbureuben4@gmail.com>
2025-04-08 00:16:30 +0300

    changes

--- src/app/graphql.ts
+++ src/app/graphql.ts
@@ -1,5 +1,26 @@
 import { gql } from "apollo-angular";
 
-export const REGISTER_USER = gql`
 
+export const REGISTER_USER = gql`
+  mutation RegisterUser(
+    $username: String!
+    $password: String!
+    $confirmPassword: String!
+    $role: String!
+  ) {
+    registerUser(
+      username: $username
+      password: $password
+      confirmPassword: $confirmPassword
+      role: $role
+    ) {
+      message
+      success
+      user {
+        id
+        isStaff
+        username
+      }
+    }
+  }
 `;
