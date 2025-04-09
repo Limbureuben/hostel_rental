c2ffc1b9edfe9bb46b6186eb13b338575de58950
Limbureuben <limbureuben4@gmail.com>
2025-04-09 13:16:48 +0300

    changes

--- src/app/sharing/sharing-routing.module.ts
+++ src/app/sharing/sharing-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserHomeComponent } from './user-home/user-home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { MapComponent } from './map/map.component';
 
 const routes: Routes = [
   { path: 'user-home', component: UserHomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'map', component: MapComponent }
 ];
 
 @NgModule({
