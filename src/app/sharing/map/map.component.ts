01bde5058eb1b6764e276bd7bd61f24d05a0f466
Limbureuben <limbureuben4@gmail.com>
2025-04-09 13:19:15 +0300

    changes

--- src/app/sharing/map/map.component.ts
+++ src/app/sharing/map/map.component.ts
@@ -8,5 +8,16 @@ import * as L from 'leaflet';
   styleUrl: './map.component.scss'
 })
 export class MapComponent {
+  ngOnInit(): void {
+    const map = L.map('map').setView([51.505, -0.09], 13); // Set initial position and zoom level
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([51.5, -0.09]).addTo(map)
+      .bindPopup('A marker')
+      .openPopup();
+  }
 
 }
