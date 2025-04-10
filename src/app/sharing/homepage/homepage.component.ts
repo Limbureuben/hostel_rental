27d4b8cee23cd3455dd065d7de525d0ea769e41d
Limbureuben <limbureuben4@gmail.com>
2025-04-10 14:16:29 +0300

    changes

--- src/app/sharing/homepage/homepage.component.ts
+++ src/app/sharing/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
@@ -8,11 +9,15 @@ import { Component } from '@angular/core';
 })
 export class HomepageComponent {
 
+  constructor(
+    private router: Router
+  ) {}
+
   toggleMenu() {
 
   }
 
   goBack() {
-    
+    this.router.navigate(['/login'])
   }
 }
