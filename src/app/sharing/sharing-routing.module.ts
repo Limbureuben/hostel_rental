dfb9aed7a31e42d32c9da91db8584fe563d80c8b
Limbureuben <limbureuben4@gmail.com>
2025-04-10 12:17:50 +0300

    changes

--- src/app/sharing/sharing-routing.module.ts
+++ src/app/sharing/sharing-routing.module.ts
@@ -4,12 +4,14 @@ import { UserHomeComponent } from './user-home/user-home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { MapComponent } from './map/map.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
   { path: 'user-home', component: UserHomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'map', component: MapComponent }
+  { path: 'homepage', component: HomepageComponent }
+  // { path: 'map', component: MapComponent }
 ];
 
 @NgModule({
