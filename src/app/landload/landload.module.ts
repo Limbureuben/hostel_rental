7437126914302237edff1aad112d70d8975a3457
Limbureuben <limbureuben4@gmail.com>
2025-04-11 21:34:29 +0300

    changes

--- src/app/landload/landload.module.ts
+++ src/app/landload/landload.module.ts
@@ -6,6 +6,19 @@ import { LandloadDashboardComponent } from './landload-dashboard/landload-dashbo
 import { LandloadHeaderComponent } from './landload-header/landload-header.component';
 import { HouseFormComponent } from './house-form/house-form.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CardModule, ButtonModule } from '@coreui/angular';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { ToastrModule } from 'ngx-toastr';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +28,23 @@ import { HouseFormComponent } from './house-form/house-form.component';
   ],
   imports: [
     CommonModule,
-    LandloadRoutingModule
+    LandloadRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    CardModule,
+    ButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatMenuModule,
+    MatOptionModule,
+    MatSelectModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-top-right',
+      timeOut: 3000
+    }),
   ]
 })
 export class LandloadModule { }
