ad4f29ca21e13d5f007b070a708a9f7666823aad
Limbureuben <limbureuben4@gmail.com>
2025-04-12 01:32:54 +0300

    changes

--- src/app/landload/house-history/house-history.component.ts
+++ src/app/landload/house-history/house-history.component.ts
@@ -3,12 +3,33 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-house-history',
   standalone: false,
   templateUrl: './house-history.component.html',
-  styleUrl: './house-history.component.scss'
+  styleUrl: './house-history.component.scss',
+  animations: [
+    // Table Pop-up Animation
+    trigger('popupAnimation', [
+      transition(':enter', [
+        style({ transform: 'scale(0.5)', opacity: 0 }),
+        animate('300ms ease-out', style({ transform: 'scale(1)', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in', style({ transform: 'scale(0.5)', opacity: 0 }))
+      ])
+    ]),
+
+    // Table Row Animation
+    trigger('rowAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-10px)' }),
+        animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+      ])
+    ])
+  ]
 })
 export class HouseHistoryComponent {
   showHouseReport = true;
@@ -23,10 +44,30 @@ export class HouseHistoryComponent {
     private dialog: MatDialog
   ) {}
 
+  ngOnInit(): void {
+    this.loadHouse()
+}
+
+loadHouse() {
+
+}
+
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
   closeReportHouseDialog() {
     this.dialogRef.close();
   }
+
+
 
 
 
