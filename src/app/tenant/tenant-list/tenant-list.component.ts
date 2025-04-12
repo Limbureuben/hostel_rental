0fc109e700fcef66f9fd377c17a3a26a3ac3e1d2
Limbureuben <limbureuben4@gmail.com>
2025-04-13 00:33:28 +0300

    changes

--- src/app/tenant/tenant-list/tenant-list.component.ts
+++ src/app/tenant/tenant-list/tenant-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-tenant-list',
@@ -9,6 +9,13 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class TenantListComponent {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialogRef: MatDialogRef<TenantListComponent>
+  ) {}
+
+  close(): void {
+    this.dialogRef.close();
+  }
 
 }
