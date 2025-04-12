703534c7d648ca0644912f98d20d52e6818c08c7
Limbureuben <limbureuben4@gmail.com>
2025-04-12 19:52:32 +0300

    changes

--- src/app/landload/landload-header/landload-header.component.ts
+++ src/app/landload/landload-header/landload-header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HouseFormComponent } from '../house-form/house-form.component';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-landload-header',
@@ -7,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrl: './landload-header.component.scss'
 })
 export class LandloadHeaderComponent {
+
+  constructor(
+    private router: Router,
+    private dialog: MatDialog
+  ) {}
+
+  onUploadRoom() {
+      this.dialog.open(HouseFormComponent, {
+        width: '900px', // 90% of the viewport width
+        maxWidth: 'none', // allow it to go wider than default max (default is 80vw)
+        panelClass: 'custom-dialog-container', // for additional styling
+        disableClose: true,
+        // Removed invalid properties
+      });
+    }
 
 }
