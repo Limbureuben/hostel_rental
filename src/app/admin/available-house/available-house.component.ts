a525529bc608f18375830cdb4347533d8d4eba02
Limbureuben <limbureuben4@gmail.com>
2025-04-13 17:23:16 +0300

    changes

--- src/app/admin/available-house/available-house.component.ts
+++ src/app/admin/available-house/available-house.component.ts
@@ -1,18 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { TenantService } from '../../services/tenant.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-available-house',
   standalone: false,
   templateUrl: './available-house.component.html',
-  styleUrl: './available-house.component.scss'
+  styleUrl: './available-house.component.scss',
+  animations: [
+    // Slide table from right when opening
+    trigger('tableEnterAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateX(50px)' }),
+        animate('500ms ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
+      ])
+    ]),
+
+    // Animate row additions & deletions
+    trigger('rowAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-10px)' }),
+        animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', style({ opacity: 0, transform: 'translateY(10px)' }))
+      ])
+    ])
+  ]
 })
 export class AvailableHouseComponent implements OnInit{
-  houses: any[] = [];
-  displayedColumns: string[] = ['house_type', 'availability_date', 'price_per_month', 'location', 'actions'];
+  // houses: any[] = [];
+
+  dataSource = new MatTableDataSource<any>([]);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(
     private router: Router,
@@ -28,11 +56,22 @@ export class AvailableHouseComponent implements OnInit{
   loadhouse() {
     this.tenantservice.getAllHouse().subscribe({
       next:(data) => {
-        this.houses = data;
+        this.dataSource.data = data;
+        this.dataSource.paginator = this.paginator;
       },
       error: (err) => {
         this.toastr.error('Failed to fetch houses', 'Error');
       }
     })
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
