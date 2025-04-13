6f45cd39db9ee4ca552457b785990ee76432279f
Limbureuben <limbureuben4@gmail.com>
2025-04-13 14:48:50 +0300

    changes

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { AvailableHouseComponent } from './available-house/available-house.component';
+import { SidebarComponent } from '@coreui/angular';
 
 const routes: Routes = [
   { path: 'admin-dashboard', component: AdminDashboardComponent },
-  { path: 'available-house', component: AvailableHouseComponent }
+  { path: 'available-house', component: AvailableHouseComponent },
+  { path: 'sidebar', component: SidebarComponent }
 ];
 
 @NgModule({
