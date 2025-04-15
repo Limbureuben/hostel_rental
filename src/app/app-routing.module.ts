9b1c7daea8a66e797729f57d6d679f9251995dac
Limbureuben <limbureuben4@gmail.com>
2025-04-15 19:08:37 +0300

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { LandloadHeaderComponent } from './landload/landload-header/landload-hea
 import { CustomerSidebarComponent } from './admin/customer-sidebar/customer-sidebar.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/user-home', pathMatch: 'full'},
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
   {
     path: 'app',
     component: HeaderComponent,
