be39ef0aa49858eae981db24e527ebbc816b5af7
Limbureuben <limbureuben4@gmail.com>
2025-04-21 23:12:54 +0300

    changes

--- src/app/tenant/tenant-routing.module.ts
+++ src/app/tenant/tenant-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TenantDashboardComponent } from './tenant-dashboard/tenant-dashboard.component';
 import { TenantOrderComponent } from './tenant-order/tenant-order.component';
+import { TenantUploadComponent } from './tenant-upload/tenant-upload.component';
 
 const routes: Routes = [
   { path: 'tenant-dashboard', component: TenantDashboardComponent },
-  { path: 'tenant-order', component: TenantOrderComponent }
+  { path: 'tenant-order', component: TenantOrderComponent },
+  { path: 'tenant-upload', component: TenantUploadComponent }
 ];
 
 @NgModule({
