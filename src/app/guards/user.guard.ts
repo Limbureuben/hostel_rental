36632ef7121f062ed416b9aec374330dcac73522
Limbureuben <limbureuben4@gmail.com>
2025-04-23 23:10:56 +0300

    changes

--- src/app/guards/user.guard.ts
+++ src/app/guards/user.guard.ts
@@ -1,5 +1,20 @@
-import { CanActivateFn } from '@angular/router';
+// user.guard.ts
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
 
-export const userGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class UserGuard implements CanActivate {
+
+  constructor(private router: Router) {}
+
+  canActivate(): boolean {
+    const isStaff = localStorage.getItem('isStaff') === 'true';
+    if (!isStaff) {
+      return true;
+    }
+    this.router.navigate(['/admin/admin-dashboard']);
+    return false;
+  }
+}
