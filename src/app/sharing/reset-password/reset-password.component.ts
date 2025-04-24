fa66238054e8500c3fb30beddcb53d43c3b5e7c2
Limbureuben <limbureuben4@gmail.com>
2025-04-24 14:47:35 +0300

    changes

--- src/app/sharing/reset-password/reset-password.component.ts
+++ src/app/sharing/reset-password/reset-password.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,6 +10,52 @@ import { Component } from '@angular/core';
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.scss'
 })
-export class ResetPasswordComponent {
+export class ResetPasswordComponent implements OnInit {
+
+  resetForm!: FormGroup;
+  uid!: string;
+  token!: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.uid = this.route.snapshot.paramMap.get('uid')!;
+    this.token = this.route.snapshot.paramMap.get('token')!;
+
+    this.resetForm = this.fb.group({
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    });
+  }
+
+  submitNewPassword(): void {
+    if (this.resetForm.valid) {
+      const { password, confirmPassword } = this.resetForm.value;
+      if (password !== confirmPassword) {
+        Swal.fire('Oops!', 'Passwords do not match.', 'error');
+        return;
+      }
+
+      this.authService.resetPassword(this.uid, this.token, password).subscribe({
+        next: () => {
+          Swal.fire({
+            icon: 'success',
+            title: 'Password reset successfully',
+            showConfirmButton: false,
+            timer: 2000
+          });
+          this.router.navigate(['/login']);
+        },
+        error: () => {
+          Swal.fire('Error', 'Invalid or expired token', 'error');
+        }
+      });
+    }
+  }
 
 }
