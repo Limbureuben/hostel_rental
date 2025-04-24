440c0379b2314d34ce77fdfd9b19f07cea7c34d9
Limbureuben <limbureuben4@gmail.com>
2025-04-25 00:24:45 +0300

    changes

--- src/app/landload/agreement-history/agreement-history.component.ts
+++ src/app/landload/agreement-history/agreement-history.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { TenantService } from '../../services/tenant.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-agreement-history',
@@ -9,6 +12,11 @@ import { TenantService } from '../../services/tenant.service';
   styleUrl: './agreement-history.component.scss'
 })
 export class AgreementHistoryComponent {
+  displayedColumns: string[] = ['from_user', 'sender_phone', 'uploaded_at', 'file', 'actions'];
+  dataSource = new MatTableDataSource<any>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   agreements: any[] = [];
 
   constructor(
@@ -21,14 +29,8 @@ export class AgreementHistoryComponent {
   }
 
   getReceivedAgreements() {
-    this.tenantService.getReceivedAgreements().subscribe({
-      next: (res) => {
-        this.agreements = res;
-      },
-      error: (err) => {
-        console.error('Failed to load agreements:', err);
-        this.toastr.error('Failed to load agreements.');
-      }
+    this.tenantService.getReceivedAgreements().subscribe(data => {
+      this.dataSource.data = data;
     });
   }
 
@@ -37,6 +39,20 @@ export class AgreementHistoryComponent {
     link.href = url;
     link.download = url.split('/').pop() ?? 'agreement.pdf'; // Extract filename or use default
     link.click();
+  }
+
+  closeDialog() {
+    this.closeDialog();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
