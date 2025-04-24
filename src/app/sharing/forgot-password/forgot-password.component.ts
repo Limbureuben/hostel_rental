7e21511ae8ae90693dc2bc58a8939a5e408828f5
Limbureuben <limbureuben4@gmail.com>
2025-04-24 12:50:02 +0300

    changes

--- src/app/sharing/forgot-password/forgot-password.component.ts
+++ src/app/sharing/forgot-password/forgot-password.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
+import { FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { response } from 'express';
+import { error } from 'console';
 
 @Component({
   selector: 'app-forgot-password',
@@ -8,13 +13,25 @@ import { Location } from '@angular/common';
   styleUrl: './forgot-password.component.scss'
 })
 export class ForgotPasswordComponent {
+  emailControl = new FormControl('', [Validators.required, Validators.email]);
 
   constructor(
-    private location: Location
+    private location: Location,
+    private authService: AuthService,
+    private snackBar: MatSnackBar,
   ) {}
 
-  submitEmail() {
-
+  submitEmail(): void {
+    if (this.emailControl.valid) {
+      this.authService.forgotPassword(this.emailControl.value).subscribe({
+        next: (response) => {
+          this.snackBar.open('Reset link sent to your email', 'Close', { duration: 3000 });
+        },
+        error: (error) => {
+          this.snackBar.open('Failed to send reset link. Please try again.', 'Close', { duration: 3000 });
+        }
+      })
+    }
   }
 
   goBack() {
