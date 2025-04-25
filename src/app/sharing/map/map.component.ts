7be344a961cf723d9accf63b15ff3a3707a52045
Limbureuben <limbureuben4@gmail.com>
2025-04-25 20:24:23 +0300

    changes

--- src/app/sharing/map/map.component.ts
+++ src/app/sharing/map/map.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
-import { Map, MapStyle, config } from '@maptiler/sdk';
+import { Map, MapStyle, Marker, config } from '@maptiler/sdk';
 import '@maptiler/sdk/dist/maptiler-sdk.css';
 
 
@@ -28,6 +28,9 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy{
       center: [initialState.lng, initialState.lat],
       zoom: initialState.zoom
     });
+    new Marker({color: "#FF0000"})
+      .setLngLat([139.7525,35.6846])
+      .addTo(this.map);
   }
 
   ngOnDestroy() {
