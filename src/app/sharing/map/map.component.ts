cb201ddb390eb87e75710a3ae47caf1184fb4832
Limbureuben <limbureuben4@gmail.com>
2025-04-25 20:47:24 +0300

    changes

--- src/app/sharing/map/map.component.ts
+++ src/app/sharing/map/map.component.ts
@@ -1,40 +1,52 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component,OnInit,ViewChild,ElementRef,AfterViewInit,OnDestroy,Inject,PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { Map, MapStyle, Marker, config } from '@maptiler/sdk';
 import '@maptiler/sdk/dist/maptiler-sdk.css';
-
 
 @Component({
   selector: 'app-map',
   standalone: false,
   templateUrl: './map.component.html',
-  styleUrl: './map.component.scss'
+  styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit, AfterViewInit, OnDestroy{
+export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
   map: Map | undefined;
+  isBrowser: boolean;
 
-  @ViewChild('map')
+  @ViewChild('map', { static: false })
   private mapContainer!: ElementRef<HTMLElement>;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
   ngOnInit(): void {
-    config.apiKey = 'YOUR_MAPTILER_API_KEY_HERE';
+    if (this.isBrowser) {
+      config.apiKey = '9rtSKNwbDOYAoeEEeW9B';
+    }
   }
 
-  ngAfterViewInit() {
-    const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
+  ngAfterViewInit(): void {
+    if (this.isBrowser) {
+      const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
 
-    this.map = new Map({
-      container: this.mapContainer.nativeElement,
-      style: MapStyle.STREETS,
-      center: [initialState.lng, initialState.lat],
-      zoom: initialState.zoom
-    });
-    new Marker({color: "#FF0000"})
-      .setLngLat([139.7525,35.6846])
-      .addTo(this.map);
+      this.map = new Map({
+        container: this.mapContainer.nativeElement,
+        style: MapStyle.STREETS,
+        center: [initialState.lng, initialState.lat],
+        zoom: initialState.zoom
+      });
+
+      new Marker({ color: '#FF0000' })
+        .setLngLat([139.7525, 35.6846])
+        .addTo(this.map);
+    }
   }
 
-  ngOnDestroy() {
-    this.map?.remove();
+  ngOnDestroy(): void {
+    if (this.isBrowser) {
+      this.map?.remove();
+    }
   }
-
 }
+
